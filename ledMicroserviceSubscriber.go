69a4740b1d01a5ffd73c96d39fe227fdac3203ad
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-05-07 16:51:25 +0100

    mqtt functionality

--- ledMicroserviceSubscriber.go
+++ ledMicroserviceSubscriber.go
@@ -4,8 +4,21 @@ import (
 	"fmt"
 	"os"
 
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	rpio "github.com/stianeikeland/go-rpio"
 )
+
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("Connected")
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	fmt.Printf("Connect lost: %v", err)
+}
 
 var ADDRESS string
 
@@ -22,9 +35,28 @@ func main() {
 		os.Exit(1)
 	}
 
-	ledPin := rpio.Pin(12)
-	ledPin.Output()
-	ledPin.Low()
+	// var broker =
+	// var port = 1883
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tcp://10.35.0.229:1883"))
+	opts.SetClientID("go_mqtt_client")
+	// opts.SetUsername("emqx")
+	// opts.SetPassword("public")
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
 
-	fmt.Println("Finished!")
+	topic := "led"
+	token := client.Subscribe(topic, 0, nil)
+	token.Wait()
+
+	// ledPin := rpio.Pin(12)
+	// ledPin.Output()
+	// ledPin.Low()
+
+	fmt.Println("Subscribed to the topic!")
 }
