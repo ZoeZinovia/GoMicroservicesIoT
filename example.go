3af54e59a6ef734bda92e2603a183596f461e1b9
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-05-10 15:38:20 +0100

    editing code

--- example.go
+++ example.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/d2r2/go-dht"
@@ -9,12 +10,12 @@ import (
 const GPIO = "GPIO4"
 
 func main() {
-	_, _, _, err :=
+	temperature, humidity, retried, err :=
 		dht.ReadDHTxxWithRetry(dht.DHT11, 4, false, 20)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// // Print temperature and humidity
-	// fmt.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
-	// 	temperature, humidity, retried)
+	// Print temperature and humidity
+	fmt.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
+		temperature, humidity, retried)
 }
