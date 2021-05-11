9e55abb6862260bb3110d304e4047c30036b6427
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-05-11 17:14:22 +0100

    l

--- humidityTemp.go
+++ humidityTemp.go
@@ -28,7 +28,7 @@ package main
 // 		while ( digitalRead( DHT_PIN ) == laststate )
 // 		{
 // 			counter++;
-// 			delayMicroseconds( 1 );
+// 			delayMicroseconds( 2 );
 // 			if ( counter == 255 )
 // 				break;
 // 		}
