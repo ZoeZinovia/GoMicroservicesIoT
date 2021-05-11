00f68abd884b4c866211f39e6113b1ffa663c6ea
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-05-11 15:23:31 +0100

    debug

--- humidityTemperatureMicroservicePublisher.go
+++ humidityTemperatureMicroservicePublisher.go
@@ -12,19 +12,25 @@ package main
 // // Reading of the dht11 is rather complex in C/C++. See this site that explains how readings are made: http://www.uugear.com/portfolio/dht11-humidity-temperature-sensor-module/
 // int* read_dht11_dat()
 // {
-//	   wiringPiSetupGpio();
+//	   wiringPiSetup();
 //     u_int8_t laststate	= HIGH;
 //     u_int8_t counter		= 0;
 //     u_int8_t j		= 0, i;
-//	   pinMode(32, OUTPUT);
-//	   digitalWrite(12, 0);
 //     dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
 //     // pull pin down for 18 milliseconds. This is called “Start Signal” and it is to ensure DHT11 has detected the signal from MCU.
-//     pinMode( DHTPIN, OUTPUT );
-//     digitalWrite( DHTPIN, LOW );
+//     pinMode(7, OUTPUT );
+//     digitalWrite(7, 0 );
+//	   FILE *f = fopen("comment.txt", "a");
+// 	   if (f == NULL)
+// 	   {
+// 	   		printf("Error opening file!\n");
+// 	   		exit(1);
+// 	   }
+//	   fprintf(f, "%s", "Managed to pull it down");
+//     fclose(f);
 //     delay( 18 );
 //     // Then MCU will pull up DATA pin for 40us to wait for DHT11’s response.
-//     digitalWrite( DHTPIN, HIGH );
+//     digitalWrite( 7, 1 );
 //     delayMicroseconds( 40 );
 //     // Prepare to read the pin
 //     pinMode( DHTPIN, INPUT );
@@ -55,9 +61,17 @@ package main
 //         }
 //     }
 //     // Check that 40 bits (8bit x 5 ) were read + verify checksum in the last byte
+//	   FILE *f = fopen("comment.txt", "a");
+// 	   if (f == NULL)
+// 	   {
+// 	   		printf("Error opening file!\n");
+// 	   		exit(1);
+// 	   }
+//	   fprintf(f, "%s", "Managed to pull it up");
+//     fclose(f);
 //     if ( (j >= 40) && (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) )
 //     {
-//		   FILE *f = fopen("file.txt", "w");
+//		   FILE *f = fopen("file.txt", "a");
 // 		   if (f == NULL)
 // 		   {
 // 		   		printf("Error opening file!\n");
@@ -67,13 +81,13 @@ package main
 //         fclose(f);
 //		   return dht11_dat; // If all ok, return pointer to the data array
 //     } else  {
-//	       FILE *f = fopen("file.txt", "w");
+//	       FILE *f = fopen("file.txt", "a");
 // 		   if (f == NULL)
 // 		   {
 // 		   		printf("Error opening file!\n");
 // 		   		exit(1);
 // 		   }
-//		   fprintf(f, "Temp: %d, %d, Humidity: %d, %d\n", dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3]);
+//		   fprintf(f, "error :( Temp: %d, %d, Humidity: %d, %d\n", dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3]);
 //		   fclose(f);
 //         dht11_dat[0] = 255;
 //         return dht11_dat; //If there was an error, set first array element to -1 as flag to main function
