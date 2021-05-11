4daf21f42e146e3e2afab13b03d75980fec8929b
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-05-11 14:49:28 +0100

    debug

--- humidityTemperatureMicroservicePublisher.go
+++ humidityTemperatureMicroservicePublisher.go
@@ -62,7 +62,8 @@ package main
 // 		   		exit(1);
 // 		   }
 //		   fprintf(f, "Temp: %d, %d, Humidity: %d, %d\n", dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3]);
-//         return dht11_dat; // If all ok, return pointer to the data array
+//         fclose(f)
+//		   return dht11_dat; // If all ok, return pointer to the data array
 //     } else  {
 //	       FILE *f = fopen("file.txt", "w");
 // 		   if (f == NULL)
@@ -70,7 +71,7 @@ package main
 // 		   		printf("Error opening file!\n");
 // 		   		exit(1);
 // 		   }
-//		   fprintf(f, "%s", "Error :(");
+//		   fprintf(f, "Temp: %d, %d, Humidity: %d, %d\n", dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3]);
 //		   fclose(f);
 //         dht11_dat[0] = 255;
 //         return dht11_dat; //If there was an error, set first array element to -1 as flag to main function
