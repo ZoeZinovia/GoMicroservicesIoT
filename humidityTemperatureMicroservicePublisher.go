68377c19d5a69c0993f7bddd38ab10267522a0b5
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-05-11 15:49:09 +0100

    debug

--- humidityTemperatureMicroservicePublisher.go
+++ humidityTemperatureMicroservicePublisher.go
@@ -4,88 +4,78 @@ package main
 // #include <wiringPi.h>
 // #include <stdio.h>
 // #include <stdlib.h>
-// #include <string.h>
-// // Pi dht11 variables
-// #define MAXTIMINGS	85
-// #define DHTPIN		7
-// int dht11_dat[5] = { 0, 0, 0, 0, 0 };
-// // Reading of the dht11 is rather complex in C/C++. See this site that explains how readings are made: http://www.uugear.com/portfolio/dht11-humidity-temperature-sensor-module/
-// int* read_dht11_dat()
+// #include <stdint.h>
+// #define MAX_TIMINGS	85
+// #define DHT_PIN		7	/* GPIO-22 */
+// int data[5] = { 0, 0, 0, 0, 0 };
+// void read_dht_data()
 // {
-//	   wiringPiSetup();
-//     u_int8_t laststate	= HIGH;
-//     u_int8_t counter		= 0;
-//     u_int8_t j		= 0, i;
-//     dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
-//     // pull pin down for 18 milliseconds. This is called “Start Signal” and it is to ensure DHT11 has detected the signal from MCU.
-//     pinMode(7, OUTPUT );
-//     digitalWrite(7, 0 );
-//     delay( 18 );
-//     // Then MCU will pull up DATA pin for 40us to wait for DHT11’s response.
-//     digitalWrite( 7, 1 );
-//     delayMicroseconds( 40 );
-//     // Prepare to read the pin
-//     pinMode( 7, INPUT );
-//     // Detect change and read data
-//     for ( i = 0; i < MAXTIMINGS; i++ )
-//     {
-//         counter = 0;
-//         while ( digitalRead( DHTPIN ) == laststate )
-//         {
-//             counter++;
-//             delayMicroseconds( 1 );
-//             if ( counter == 255 )
-//             {
-//                 break;
-//             }
-//         }
-//         laststate = digitalRead( 7 );
-//         if ( counter == 255 )
-//             break;
-//         // Ignore first 3 transitions
-//         if ( (i >= 4) && (i % 2 == 0) )
-//         {
-//             // Add each bit into the storage bytes
-//             dht11_dat[j / 8] <<= 1;
-//             if ( counter > 16 )
-//                 dht11_dat[j / 8] |= 1;
-//             j++;
-//			   FILE *f = fopen("comment.txt", "a");
-// 	   			if (f == NULL)
-// 	  			{
-// 	   				printf("Error opening file!\n");
-// 	   				exit(1);
-// 	   			}
-//	   			fprintf(f, "reached j loop, %d\n", j);
-//	   			fprintf(f, "dht value: , %d, %d, %d, %d, %d\n", dht11_dat[0],dht11_dat[1],dht11_dat[2],dht11_dat[3],dht11_dat[4]);
-//	   			fprintf(f, "dht array: , %d\n", dht11_dat[j/8]);
-//     			fclose(f);
-//         }
-//     }
-//     // Check that 40 bits (8bit x 5 ) were read + verify checksum in the last byte
-//     if ( (j >= 40) && (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) )
-//     {
-//		   FILE *f = fopen("file.txt", "a");
-// 		   if (f == NULL)
-// 		   {
-// 		   		printf("Error opening file!\n");
-// 		   		exit(1);
-// 		   }
-//		   fprintf(f, "Temp: %d, %d, Humidity: %d, %d\n", dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3]);
-//         fclose(f);
-//		   return dht11_dat; // If all ok, return pointer to the data array
-//     } else  {
-//	       FILE *f = fopen("file.txt", "a");
-// 		   if (f == NULL)
-// 		   {
-// 		   		printf("Error opening file!\n");
-// 		   		exit(1);
-// 		   }
-//		   fprintf(f, "error :( Temp: %d, %d, Humidity: %d, %d\n", dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3]);
-//		   fclose(f);
-//         dht11_dat[0] = 255;
-//         return dht11_dat; //If there was an error, set first array element to -1 as flag to main function
-//     }
+// 	uint8_t laststate	= HIGH;
+// 	uint8_t counter		= 0;
+// 	uint8_t j			= 0, i;
+// 	data[0] = data[1] = data[2] = data[3] = data[4] = 0;
+// 	/* pull pin down for 18 milliseconds */
+// 	pinMode( DHT_PIN, OUTPUT );
+// 	digitalWrite( DHT_PIN, LOW );
+// 	delay( 18 );
+// 	/* prepare to read the pin */
+// 	pinMode( DHT_PIN, INPUT );
+// 	/* detect change and read data */
+// 	for ( i = 0; i < MAX_TIMINGS; i++ )
+// 	{
+// 		counter = 0;
+// 		while ( digitalRead( DHT_PIN ) == laststate )
+// 		{
+// 			counter++;
+// 			delayMicroseconds( 1 );
+// 			if ( counter == 255 )
+// 			{
+// 				break;
+// 			}
+// 		}
+// 		laststate = digitalRead( DHT_PIN );
+// 		if ( counter == 255 )
+// 			break;
+// 		/* ignore first 3 transitions */
+// 		if ( (i >= 4) && (i % 2 == 0) )
+// 		{
+// 			/* shove each bit into the storage bytes */
+// 			data[j / 8] <<= 1;
+// 			if ( counter > 16 )
+// 				data[j / 8] |= 1;
+// 			j++;
+// 		}
+// 	}
+// 	/*
+// 	 * check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
+// 	 * print it out if data is good
+// 	 */
+// 	if ( (j >= 40) &&
+// 	     (data[4] == ( (data[0] + data[1] + data[2] + data[3]) & 0xFF) ) )
+// 	{
+// 		float h = (float)((data[0] << 8) + data[1]) / 10;
+// 		if ( h > 100 )
+// 		{
+// 			h = data[0];	// for DHT11
+// 		}
+// 		float c = (float)(((data[2] & 0x7F) << 8) + data[3]) / 10;
+// 		if ( c > 125 )
+// 		{
+// 			c = data[2];	// for DHT11
+// 		}
+// 		if ( data[2] & 0x80 )
+// 		{
+// 			c = -c;
+// 		}
+// 		float f = c * 1.8f + 32;
+//		FILE *f = fopen("comment.txt", "a");
+// 		fprintf(f, "Humidity = %.1f %% Temperature = %.1f *C (%.1f *F)\n", h, c, f );
+//		fclose();
+// 	}else  {
+//		FILE *f = fopen("comment.txt", "a");
+// 		fprintf(f, "%s", "Data not good, skip\n" );
+//		fclose();
+// 	}
 // }
 import "C"
 
