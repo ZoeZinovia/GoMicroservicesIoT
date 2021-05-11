3fa3b4572ef0b4b12a578fe2e785ec4c82d43576
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-05-11 10:36:29 +0100

    debug

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 // #include <stdio.h>
+// #include <stdlib.h>
 // int greet(const char *name, int year) {
 //     int n = 2;
 //     printf("Greetings, %s from %d! We come in peace :)", name, year);
