f7e6967e55d05696ffd55acb001d1fbfe3027c9a
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-05-11 10:32:20 +0100

    debug

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-// #cgo CFLAGS: -g -Wall
-// #include <stdlib.h>
-// #include "greeter.h"
+// #include <stdio.h>
+
+// int greet(const char *name, int year) {
+//     int n = 2;
+
+//     printf("Greetings, %s from %d! We come in peace :)", name, year);
+
+//     return n;
+// }
 import "C"
 
 import (
