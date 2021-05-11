3fe36056269e063565d0a8a4d08c0ebc907250ca
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-05-11 14:52:38 +0100

    debug

--- humidityTemperatureMicroservicePublisher.go
+++ humidityTemperatureMicroservicePublisher.go
@@ -8,6 +8,8 @@ package main
 // // Pi dht11 variables
 // #define MAXTIMINGS	85
 // #define DHTPIN		7
+
+// printf("DHT PIN: %d", DHTPIN)
 // int dht11_dat[5] = { 0, 0, 0, 0, 0 };
 // // Reading of the dht11 is rather complex in C/C++. See this site that explains how readings are made: http://www.uugear.com/portfolio/dht11-humidity-temperature-sensor-module/
 // int* read_dht11_dat()
