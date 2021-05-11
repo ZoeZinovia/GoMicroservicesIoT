9f6155e7471478de41bd0ec75569528032c2f8da
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-05-11 10:50:10 +0100

    debugging dht

--- humidityTemperatureMicroservicePublisher.go
+++ humidityTemperatureMicroservicePublisher.go
@@ -1,10 +1,63 @@
 package main
 
+// #include <wiringPi.h>
 // #include <stdio.h>
 // #include <stdlib.h>
-// int greet(const char *name, int year) {
-//     int n = 2;
-//     return n;
+// #include <string.h>
+// // Pi dht11 variables
+// #define MAXTIMINGS	85
+// #define DHTPIN		4
+// int dht11_dat[5] = { 0, 0, 0, 0, 0 };
+// // Reading of the dht11 is rather complex in C/C++. See this site that explains how readings are made: http://www.uugear.com/portfolio/dht11-humidity-temperature-sensor-module/
+// int* read_dht11_dat()
+// {
+//     u_int8_t laststate	= HIGH;
+//     u_int8_t counter		= 0;
+//     u_int8_t j		= 0, i;
+//     dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+//     // pull pin down for 18 milliseconds. This is called “Start Signal” and it is to ensure DHT11 has detected the signal from MCU.
+//     pinMode( DHTPIN, OUTPUT );
+//     digitalWrite( DHTPIN, LOW );
+//     delay( 18 );
+//     // Then MCU will pull up DATA pin for 40us to wait for DHT11’s response.
+//     digitalWrite( DHTPIN, HIGH );
+//     delayMicroseconds( 40 );
+//     // Prepare to read the pin
+//     pinMode( DHTPIN, INPUT );
+//     // Detect change and read data
+//     for ( i = 0; i < MAXTIMINGS; i++ )
+//     {
+//         counter = 0;
+//         while ( digitalRead( DHTPIN ) == laststate )
+//         {
+//             counter++;
+//             delayMicroseconds( 1 );
+//             if ( counter == 255 )
+//             {
+//                 break;
+//             }
+//         }
+//         laststate = digitalRead( DHTPIN );
+//         if ( counter == 255 )
+//             break;
+//         // Ignore first 3 transitions
+//         if ( (i >= 4) && (i % 2 == 0) )
+//         {
+//             // Add each bit into the storage bytes
+//             dht11_dat[j / 8] <<= 1;
+//             if ( counter > 16 )
+//                 dht11_dat[j / 8] |= 1;
+//             j++;
+//         }
+//     }
+//     // Check that 40 bits (8bit x 5 ) were read + verify checksum in the last byte
+//     if ( (j >= 40) && (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) )
+//     {
+//         return dht11_dat; // If all ok, return pointer to the data array
+//     } else  {
+//         dht11_dat[0] = -1;
+//         return dht11_dat; //If there was an error, set first array element to -1 as flag to main function
+//     }
 // }
 import "C"
 
@@ -16,7 +69,6 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
-	"unsafe"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
@@ -54,14 +106,8 @@ func publish(client mqtt.Client) {
 	// 	log.Fatal(err)
 	// }
 
-	name := C.CString("Gopher")
-	defer C.free(unsafe.Pointer(name))
-
-	year := C.int(2018)
-	number := C.greet(name, year)
-	fmt.Println(number)
-	// dataReadings := C.read_dht11_dat()
-	// fmt.Printf("%T\n", dataReadings)
+	dataReadings := C.read_dht11_dat()
+	fmt.Printf("%T\n", dataReadings)
 
 	// currentTemperature := tempStruct{
 	// 	Temp: temperatureReading,
