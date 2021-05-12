e7f255fa8e1a6db75e8f0a70a87812f68a4ad4c4
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-05-12 16:26:23 +0100

    j

--- humidityTemperatureMicroservicePublisher.go
+++ humidityTemperatureMicroservicePublisher.go
@@ -34,7 +34,7 @@ package main
 // 		while ( digitalRead( DHT_PIN ) == laststate )
 // 		{
 // 			counter++;
-// 			delayMicroseconds( 2 );
+// 			delayMicroseconds( 1 );
 // 			if ( counter == 255 )
 // 				break;
 // 		}
